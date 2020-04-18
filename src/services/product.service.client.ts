d3c1b0592950ad2e898b06630a73f7847cc24e60
ElliotLiu <liu.yic@husky.neu.edu>
2020-04-17 22:33:42 -0400

    'third'

--- src/services/product.service.client.ts
+++ src/services/product.service.client.ts
@@ -21,7 +21,7 @@ export class ProductService {
   }
 
   findAllProduct(): Observable<Product[]> {
-    return this.http.get<Product[]>('https://cs5200projectlyc.herokuapp.com/findAllProduct/')
+    return this.http.get<Product[]>('https://cs5200projectlyc.herokuapp.com/api/findAllProduct/')
 
   }
 
